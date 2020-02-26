65f0772d1db852de32dd5b02d5ab47d4b66844b2
iuryChristmas <iury.christmas@gmail.com>
2020-02-25 23:23:04 -0300

    criacao do menu e navegacao entre paginas, criado arquivos onde ficara a lista e o form

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>A noite chega, e agora começa a minha vigia. Não terminará até a minha morte.</p>
+        <h2>Missão e Visão</h2>
+        <p>Não tomarei esposa, não possuirei terras, não gerarei filhos. Não usarei coroas e não conquistarei glórias.</p>
+        <p>Viverei e morrerei no meu posto. Sou a espada na escuridão. Sou o vigilante nas muralhas.</p>
+        <p>Sou o fogo que arde contra o frio, a luz que traz consigo a alvorada, a trombeta que acorda os que dormem, o escudo que defende os reinos dos homens.</p>
+        <p>Dou a minha vida e a minha honra à Patrulha da Noite, por esta noite e por todas as noites que estão para vir.</p>
     </div>
 )
