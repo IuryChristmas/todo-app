0af8aa5f0dc1f90784d53410c7f9884393040eb2
iuryChristmas <iury.christmas@gmail.com>
2020-03-14 15:31:40 -0300

    iniciada integracao com o redux e configuracao do redux devtools

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
-
-export default props => {
+const TodoForm = props => {
 
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
@@ -17,7 +20,7 @@ export default props => {
         <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control' placeholder='Adicione uma tarefa' 
-                onChange={props.handleChange} onKeyUp={keyHandler} value={props.description}></input>
+                onChange={props.changeDescription} onKeyUp={keyHandler} value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
@@ -27,3 +30,7 @@ export default props => {
     </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
